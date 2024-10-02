53a22bc81139aaca8db8bd438ed497ab547b3751
Kevin <juandeag74@outlook.com>
2024-10-02 15:34:51 +0200

    Setup the database and added connection in server.js - also fixed couple issues

--- Containers/Backend/app/server.js
+++ Containers/Backend/app/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const cors = require('cors');
 const port = 5000; //Port for backend
 
 app.use(cors()); 
 app.use(express.json()); 
+
+//Create connection to the database
+const connection = mysql.createConnection({
+    host: 'mysql',
+    user: 'user',
+    password: 'BTHDonken',
+    database: 'burger_db'
+});
+
+//Connect to the database
+connection.connect((err) => {
+    if (err) {
+        console.error('Failed to connect to the database, error:', err);
+        return;
+    }
+    console.log('Successfully connected to BTHDonkens database!');
+});
 
 //Post request for creating the order
 app.post('/api/order', (req, res) => {

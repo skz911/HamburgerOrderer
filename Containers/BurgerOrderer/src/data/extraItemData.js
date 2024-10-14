8a691700b6d9c97c781b62b042ff40ff66a9e935
Linus-web <sunil.andersson@gmail.com>
2024-10-14 21:54:02 +0200

    fixed some static files for ingredients drinks and extra items and worked together with backend to fix how the json package should look like

--- Containers/BurgerOrderer/src/data/extraItemData.js
+++ Containers/BurgerOrderer/src/data/extraItemData.js
@@ -1,62 +1,36 @@
 const extraItems = {
-    extra_items: [
+    "extra_items": [
       {
-        id: 1,
-        name: "Fries",
-        type: "side",
-        price: 10,
-        image: "/path-to-fries-image",
+        "id": 1,
+        "name": "Fries",
+        "price": 15,
+        "image": "/assets/fries.png"
       },
       {
-        id: 2,
-        name: "Onion Rings",
-        type: "side",
-        price: 12,
-        image: "/path-to-onion-rings-image",
+        "id": 2,
+        "name": "Onion Rings",
+        "price": 18,
+        "image": "/assets/onion_rings.png"
       },
       {
-        id: 3,
-        name: "Mozzarella Sticks",
-        type: "side",
-        price: 15,
-        image: "/path-to-mozzarella-sticks-image",
+        "id": 3,
+        "name": "Chicken Nuggets",
+        "price": 20,
+        "image": "/assets/nuggets.png"
       },
       {
-        id: 4,
-        name: "BBQ Sauce",
-        type: "sauce",
-        price: 3,
-        image: "/path-to-bbq-sauce-image",
+        "id": 4,
+        "name": "Mozzarella Sticks",
+        "price": 22,
+        "image": "/assets/mozzarella_sticks.png"
       },
       {
-        id: 5,
-        name: "Ketchup",
-        type: "sauce",
-        price: 2,
-        image: "/path-to-ketchup-image",
-      },
-      {
-        id: 6,
-        name: "Mayonnaise",
-        type: "sauce",
-        price: 3,
-        image: "/path-to-mayonnaise-image",
-      },
-      {
-        id: 7,
-        name: "Garlic Dip",
-        type: "sauce",
-        price: 4,
-        image: "/path-to-garlic-dip-image",
-      },
-      {
-        id: 8,
-        name: "Cheese Dip",
-        type: "sauce",
-        price: 5,
-        image: "/path-to-cheese-dip-image",
-      },
-    ],
+        "id": 5,
+        "name": "Side Salad",
+        "price": 10,
+        "image": "/assets/side_salad.png"
+      }
+    ]
   };
 
   export default extraItems;
